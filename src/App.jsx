73bd78f9515c38f5b31fbe3694fb6857d262e267
M <mt.dev.2023@gmail.com>
2024-11-16 03:36:45 +0100

    7.30

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
+import {useEffect, useStat} from "react"
 import loader from "./assets/loader.svg"
 import "./App.css"
+import { useEffect } from "react"
+import { useState } from "react"
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
 
 function App() {
-   return (
 
-      <main>
-        <div className="loader-container">
-          <img src={loader} alt="loader icon" />
-        </div>
-        <div className="container">
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key={{YOUR_API_KEY}}`)
+  }, [] )
+
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loader icon" />
+      </div>
+      <div className="container">
         <div className="info-icon-container">
-          <img src="/icons/01d.svg" className="info-icon" alt="weather icon"/>
+          <img src="/icons/01d.svg" className="info-icon" alt="weather icon" />
         </div>
         <p className="temperature">24°</p>
         <p className="city-name">Montpellier</p>
         <p className="country-name">France</p>
-        
-        
-        
-        
-        
-        </div>
-      </main>
-  
+      </div>
+    </main>
   );
 }
 
